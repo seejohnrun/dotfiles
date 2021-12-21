f57f66ad1af9c3e19ab2140cae16f9780abb6f66
John Crepezzi <john.crepezzi@gmail.com>
2021-12-21 11:58:56 -0500

    Fix ordering

--- install.sh
+++ install.sh
@@ -4,11 +4,11 @@
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
-# install the plugs
-vim +PlugInstall +qall > /dev/null
-
 # symlink the dotfiles
 wd=$(pwd)
 ln -sf $wd/.vimrc ~/.vimrc
 ln -sf $wd/.tmux.conf ~/.tmux.conf
 ln -sf $wd/.zshrc ~/.zshrc
+
+# install the plugs
+vim +PlugInstall +qall > /dev/null
