cc9a75c6c14a9d7242bc6a74b0154a11c129b418
John Crepezzi <john.crepezzi@gmail.com>
2021-12-21 11:56:02 -0500

    Remove dotfiles script for reattach-to-user-namespace

--- install.sh
+++ install.sh
@@ -5,11 +5,10 @@ curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 # install the plugs
-vim +PlugInstall +qall
+vim +PlugInstall +qall > /dev/null
 
 # symlink the dotfiles
 wd=$(pwd)
 ln -sf $wd/.vimrc ~/.vimrc
 ln -sf $wd/.tmux.conf ~/.tmux.conf
 ln -sf $wd/.zshrc ~/.zshrc
-ln -sf $wd/dotfiles-scripts ~/dotfiles-scripts
